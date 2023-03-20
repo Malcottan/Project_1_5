c38a8a5ad1a704148a4c026c88c5eb77fa98df62
Malcottan <maxtryasiy@gmail.com>
2023-03-20 11:14:33 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x;
+    float y;
+    float q;
+    cout << "Enter the number(x): ";
+    cin >> x;
+    cout << "Enter the number(y): ";
+    cin >> y;
+    cout << "Enter the number(q): ";
+    cin >> q;
+    cout << "Output number(x+y+q): " << (x+y+q) << endl;
+    cout << "Output number(x*y*q): " << (x*y*q) << endl;
+    return 0;
+}
